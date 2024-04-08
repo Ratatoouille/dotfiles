c365a7ed76bfab5905937b0034c9f0e7a021577b
Aleksey Startsev <startsev@bifit.com>
2024-04-09 01:38:13 +0700

    telescope

--- nvim/lua/ratatouille/plugins/telescope.lua
+++ nvim/lua/ratatouille/plugins/telescope.lua
@@ -40,7 +40,7 @@ return {
         -- set keymaps
         local keymap = vim.keymap -- for conciseness
 
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {
+        keymap.set("n", "<leader>tf", "<cmd>Telescope find_files<cr>", {
             desc = "Fuzzy find files in cwd"
         })
         keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {
